183746ab0460242fbabd7a0012f2435b336662d8
joejourden <joejourden@gmail.com>
2022-03-01 16:53:59 -0500

    completed code

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # em-methods-project
-This repo contains the code and simulated data for my final project for the course Empirical Methods for Applied Microeconomics.
+This repo contains code from my final project for the course Empirical Methods for Applied Microeconomics.
 I have updated the code to improve readability.
 
 I keep the simulated data presented in the paper in the folder "data-original". If the R scripts are run, the folder
@@ -9,4 +9,4 @@ I keep the simulated data presented in the paper in the folder "data-original". 
 2. functions for DA and DACC algorithms
 3. main block runs DACC 100 times per market, exports pdf document summarizing results
 
-#### To change number of men/women, edit 'choose_MW.csv' in data folder. (default M=20, W=25)
+# To change number of men/women in preference data, edit 'choose_MW.csv' in data folder. (default M=20, W=25)
--- scripts/temp_debugging.R
+++ scripts/temp_debugging.R
@@ -45,3 +45,48 @@ typeof(A_M); typeof(A_W); typeof(Budget_M); typeof(Budget_W); typeof(Mu)
 
 
 
+M <- 5
+W <- 6
+err <- rlogis(M, location = 0, scale = 1)
+for (k in 2:W){
+  err <- cbind(err,rlogis(M, location = 0, scale = 1))
+}
+age_M <- rnorm(M, mean = 26, sd = 2)
+income_M <- rnorm(M, mean = 50, sd = 10)
+height_M <- rnorm(M, mean = 70.6, sd = 2)
+bmi_M <- rnorm(M, mean = 26, sd = 2)
+bmi2_M <- bmi_M^2
+
+age_W <- rnorm(W, mean = 25, sd = 2)
+income_W <- rnorm(W, mean = 50, sd = 10)
+height_W <- rnorm(W, mean = 65, sd = 2)
+bmi_W <- rnorm(W, mean = 22.8, sd = 2)
+bmi2_W <- bmi_W^2
+
+D_M <- cbind(age_M,income_M,height_M,bmi_M,bmi2_M)
+D_W <- cbind(age_W,income_W,height_W,bmi_W,bmi2_W)
+
+beta_M <- as.numeric(c(-0.0598, 0.0053, -0.1421, -0.3962, 0.0043))
+beta_W <- as.numeric(c(-0.0098, 0.0164, 0.1831, 0.1332, -0.0007))
+
+# U_M[m,w] is man m's utility from matching with woman w
+U_M <- matrix(nrow = M, ncol = W)
+for(m in 1:M){
+  for(w in 1:W){
+    U_M[m,w] = D_W[w,]%*%beta_M + err[m,w]
+  }
+}
+# U_W[w,m] is woman w's utility from matching with man m
+U_W <- matrix(nrow = W, ncol = M)
+for(w in 1:W){
+  for(m in 1:M){
+    U_W[w,m] = D_M[m,]%*%beta_W + err[m,w]
+  }
+}
+
+# Men's preference matrix
+# the (m,w) entry is man m's ranking of woman w
+pref_M <- t(apply(-U_M, 1, function(x) rank(x, ties.method = "random")))
+# Women's preference matrix
+# the (w,m) entry is woman w's ranking of man m
+pref_W <- t(apply(-U_W, 1, function(x) rank(x, ties.method = "random")))
